c88d6cae25dbb7eab98c3ff7eee0a806a5e8a947
Ashish <ashishiit9@gmail.com>
2024-11-20 13:36:57 +0530

    week 1 tasks

---
+++ Week1/src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Josys Week 1 Assignments</h1>
+    </div>
+  );
+}
+
+export default App;
